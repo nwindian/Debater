46f7713ce82113ed230bdb620984ff3df1642f84
beckyparkes <rebekahmparkes@outlook.com>
2023-03-09 22:37:53 -0500

    gitignore ./next, /.next/, .next, **/.next, one of these has to work

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e83915861017abeb"; }
+/******/ 		__webpack_require__.h = function() { return "eb08050089ad1310"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
