b9ff3a83ed70fe725e54a2b4ef138bd085ed05c4
beckyparkes <rebekahmparkes@outlook.com>
2023-03-11 15:15:05 -0500

    Basic components/debate-message styles; colors and alignment conditional on voting side

--- src/components/debate-components/debate-body.js
+++ src/components/debate-components/debate-body.js
@@ -1,11 +1,15 @@
-import styles from '@/styles/Home.module.scss'
+import { Grid } from '@nextui-org/react'
 import moment from 'moment/moment'
 import DebateMessage from './debate-message'
 
-const DebateBody = ({ title, messages }) => {
-    return messages.length && messages.map((m) => {
-        const time = moment().subtract(m.timeAgoPosted[0], 'days').subtract(m.timeAgoPosted[1], 'hours').calendar()
-        return <DebateMessage key={m.id} id={m.id} user={m.user} body={m.body} time={time} side={m.sideIsPro ? "pro" : "against"} />
-    })
+const DebateBody = ({ messages }) => {
+    return (
+        <Grid.Container gap={1}>
+            {messages.length && messages.map((m) => {
+                const time = moment().subtract(m.timeAgoPosted[0], 'days').subtract(m.timeAgoPosted[1], 'hours').calendar()
+                return <DebateMessage key={m.id} id={m.id} user={m.user} body={m.body} time={time} side={m.sideIsPro ? "pro" : "against"} />
+            })}
+        </Grid.Container>
+    )
 }
 export default DebateBody
--- src/pages/debate/[vote].js
+++ src/pages/debate/[vote].js
@@ -1,4 +1,4 @@
-import { Container, Row, Text } from "@nextui-org/react";
+import { Col, Container, Row, Text, Card } from "@nextui-org/react";
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import DebateBody from '@/components/debate-components/debate-body'
@@ -21,12 +21,16 @@ const Debate = () => {
 
     return (
         <Container fluid>
-            <Row justify="center" align="center">
-                {data && <Text>{data.title}</Text>}
-            </Row>
-            <p>Your Vote: {vote}</p>
+            <Col>
+                <Card variant="bordered">
+                    {data && <Text>{data.title}</Text>}
+                </Card>
+                <Card variant="bordered">
+                    <Text>Your Vote: {vote}</Text>
+                </Card>
+            </Col>
             {data && <DebateBody title={data.title} messages={data.messages} />}
-            <Link style={{border: '2px solid red'}} href={`/discussion/${vote}`}>Go to {vote === "agree" ? "pro" : "against"} discussion</Link>
+            <Link style={{ border: '2px solid red' }} href={`/discussion/${vote}`}>Go to {vote === "agree" ? "pro" : "against"} discussion</Link>
         </Container>
     )
 }
