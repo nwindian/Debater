e12c88c2b3b1b424e4ac978a78f73ae5e996c81e
beckyparkes <rebekahmparkes@outlook.com>
2023-03-14 21:37:43 -0400

    helpers.js file has helper func to generate random messages, call in discussion and get-debate api routes

--- src/pages/api/discussion/[side].js
+++ src/pages/api/discussion/[side].js
@@ -1,16 +1,18 @@
-// GET discussion
+const { generateRandomDiscussion } = require('../../../helpers')
 
 export default function handler(req, res) {
     const { side } = req.query
-    res.status(200).json({
-        status: 200,
-        message: "discussion loaded",
-        discussion: {
-            side: "pro",
-            messages: [
-                { id: 0, user: "user-1", body: "here's a reason why I agree" },
-                { id: 1, user: "user-2", body: "here's a a response to your message" }
-            ]
-        }
-    })
+    const messages = generateRandomDiscussion(15)
+
+    side === "agree" ?
+        res.status(200).json({
+            status: 200,
+            message: "discussion loaded",
+            discussion: { side: "agree", messages }
+        }) :
+        res.status(200).json({
+            status: 200,
+            message: "discussion loaded",
+            discussion: { side: "against", messages }
+        })
 }
--- src/pages/api/get-debate.js
+++ src/pages/api/get-debate.js
@@ -1,19 +1,11 @@
+const { generateRandomDiscussion } = require('../../helpers')
+
 export default function handler(req, res) {
-      const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-      const messageBody = () => loremIpsum.slice(0, Math.floor(Math.random() * loremIpsum.length))
-      const messages = Array.from({length: 10}, (e, i) => {
-            return {
-                  id: i,
-                  user: "user" + i,
-                  body: messageBody(),
-                  sideIsPro: i % 2 === 0 ? true : false,
-                  timeAgoPosted: [4, 1.5]
-            }
-      })
+      const messages = generateRandomDiscussion(10)
 
       res.status(200).json({
             status: 200,
             message: "debate retrieved",
-            debate: { title: "A Debate Title", messages}
+            debate: { title: "A Debate Title", messages }
       })
 }
