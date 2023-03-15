25cfbb766f6357d70fb2cedf2d88a553ce9d9ce4
beckyparkes <rebekahmparkes@outlook.com>
2023-03-14 23:56:22 -0400

    add ugly like and reply buttons on messages

--- src/components/debate-components/debate-message.js
+++ src/components/debate-components/debate-message.js
@@ -1,12 +1,17 @@
 import { useRouter } from 'next/router';
-import { Text, Card } from "@nextui-org/react";
+import { Text, Card, Row, Button } from "@nextui-org/react";
+import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
+import { BsReply, BsReplyFill } from 'react-icons/bs'
+import { useState } from 'react';
 
 const DebateMessage = ({ id, user, body, time, side }) => {
     const router = useRouter()
+    const [isLiked, setIsLiked] = useState(false)
+    const [reply, setReply] = useState(false)
 
     const styles = side === "agree" ?
         { headerBg: "#4E041E", bodyBg: "#910838", userTxt: "#B80A47", timeTxt: "#910838", bodyTxt: "#FDD8E5", justify: "flex-start", bodyTxtAlign: "left", cardMargin: ".2% 5% .2% 0" } :
-        { headerBg: "#FDD8E5", bodyBg: "#FEF0F5", userTxt: "#F31260", timeTxt: "#F881AB", bodyTxt: "#4E041E", justify: "flex-end", bodyTxtAlign: "right", cardMargin: ".2% 0 .2% 5%"}
+        { headerBg: "#FDD8E5", bodyBg: "#FEF0F5", userTxt: "#F31260", timeTxt: "#F881AB", bodyTxt: "#4E041E", justify: "flex-end", bodyTxtAlign: "right", cardMargin: ".2% 0 .2% 5%" }
 
     return (
         <Card
@@ -20,9 +25,15 @@ const DebateMessage = ({ id, user, body, time, side }) => {
                 <Text css={{ color: styles.userTxt }}>{user}</Text>
                 <Text css={{ color: styles.timeTxt, marginLeft: "2%" }}>{time}</Text>
             </Card.Header>
-            <Card.Body css={{ backgroundColor: styles.bodyBg }}>
+            <Card.Body css={{ backgroundColor: styles.bodyBg, paddingBottom: 0, paddingTop: "1%" }}>
                 <Text css={{ color: styles.bodyTxt, textAlign: styles.bodyTxtAlign }}>{body}</Text>
             </Card.Body>
+            <Card.Footer css={{ backgroundColor: styles.bodyBg, padding: 0 }}>
+                <Row justify="space-evenly">
+                    <Button onPress={() => setIsLiked(!isLiked)} css={{ background: "inherit", color: styles.bodyTxt }} icon={isLiked ? <AiFillLike /> : <AiOutlineLike />} />
+                    <Button onPress={() => setReply(!reply)} css={{ background: "inherit", color: styles.bodyTxt }} icon={reply ? < BsReplyFill /> : <BsReply />} />
+                </Row>
+            </Card.Footer>
         </Card>
     )
 }
--- src/components/discussion-components/discussion-message.js
+++ src/components/discussion-components/discussion-message.js
@@ -1,15 +1,18 @@
 import { useRouter } from 'next/router';
-import { Text, Card } from "@nextui-org/react";
-import { useRef, useEffect } from 'react';
+import { Text, Card, Row, Button } from "@nextui-org/react";
+import { useRef, useEffect, useState } from 'react';
+import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
+import { BsReply, BsReplyFill } from 'react-icons/bs'
 
 const DiscussionMessage = ({ user, body, id, side, time, focused }) => {
-    console.log(focused, "focused")
     const messageRef = useRef(null)
+    const [isLiked, setIsLiked] = useState(false)
+    const [reply, setReply] = useState(false)
 
-    useEffect(()=>{
+    useEffect(() => {
         if (focused) messageRef.current.focus()
     })
-    
+
     const router = useRouter()
     const styles = side === "agree" ?
         { headerBg: "#4E041E", bodyBg: "#910838", userTxt: "#B80A47", timeTxt: "#910838", bodyTxt: "#FDD8E5", justify: "flex-start", bodyTxtAlign: "left", cardMargin: ".2% 5% .2% 0" } :
@@ -28,9 +31,15 @@ const DiscussionMessage = ({ user, body, id, side, time, focused }) => {
                 <Text css={{ color: styles.userTxt }}>{user}</Text>
                 <Text css={{ color: styles.timeTxt, marginLeft: "2%" }}>{time}</Text>
             </Card.Header>
-            <Card.Body css={{ backgroundColor: styles.bodyBg }}>
+            <Card.Body css={{ backgroundColor: styles.bodyBg, paddingBottom: 0, paddingTop: "1%" }}>
                 <Text css={{ color: styles.bodyTxt, textAlign: styles.bodyTxtAlign }}>{body}</Text>
             </Card.Body>
+            <Card.Footer css={{ backgroundColor: styles.bodyBg, padding: 0 }}>
+                <Row justify="space-evenly">
+                    <Button onPress={() => setIsLiked(!isLiked)} css={{ background: "inherit", color: styles.bodyTxt }} icon={isLiked ? <AiFillLike /> : <AiOutlineLike />} />
+                    <Button onPress={() => setReply(!reply)} css={{ background: "inherit", color: styles.bodyTxt }} icon={reply ? < BsReplyFill /> : <BsReply />} />
+                </Row>
+            </Card.Footer>
         </Card>
     )
 }
