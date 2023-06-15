c683eb4ef5274ab7774eeee4c2299b6305ecb1d3
beckyparkes <rebekahmparkes@outlook.com>
2023-06-15 10:51:37 -0500

    fix bug on helpers.js using ESM syntax

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 import moment from 'moment/moment'
 
 // function that generates an array of messages, of length arrayLength 
-const generateRandomDiscussion = (arrayLength) => {
+export const generateRandomDiscussion = (arrayLength) => {
     const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     const messageBody = () => loremIpsum.slice(0, Math.floor(Math.random() * loremIpsum.length))
     const messages = Array.from({ length: arrayLength }, (e, i) => {
@@ -18,6 +18,6 @@ const generateRandomDiscussion = (arrayLength) => {
 }
 
 // function that gets and formats time of message posted
-const getTime = (days, hours) => moment().subtract(days, 'days').subtract(hours, 'hours').calendar()
+export const getTime = (days, hours) => moment().subtract(days, 'days').subtract(hours, 'hours').calendar()
 
-module.exports = { generateRandomDiscussion, getTime }
+
